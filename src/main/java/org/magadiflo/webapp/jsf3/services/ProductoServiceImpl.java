6da8b83a3b58e825b21b6a8e79c01f4315c3c05c
Martín <magadiflo@gmail.com>
2022-02-02 17:08:15 -0500

    Implememntando seguridad JAAS en aplicación web empresarial (EJB y JSF3).  Lógin básico con popup del navegador. Los usuarios ser crearon en el servidor WildFly junto con sus roles ADMIN y USER

--- src/main/java/org/magadiflo/webapp/jsf3/services/ProductoServiceImpl.java
+++ src/main/java/org/magadiflo/webapp/jsf3/services/ProductoServiceImpl.java
@@ -1,5 +1,8 @@
 package org.magadiflo.webapp.jsf3.services;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import org.magadiflo.webapp.jsf3.entities.Categoria;
@@ -11,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Stateless
+@DeclareRoles({"USER", "ADMIN"})
 public class ProductoServiceImpl implements ProductoService {
 
     @Inject
@@ -19,36 +23,43 @@ public class ProductoServiceImpl implements ProductoService {
     @Inject
     private CrudRepository<Categoria> categoriaCrudRepository;
 
+    @PermitAll
     @Override
     public List<Producto> listar() {
         return this.productoCrudRepository.listar();
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public Optional<Producto> porId(Long id) {
         return Optional.ofNullable(this.productoCrudRepository.porId(id));
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void guardar(Producto p) {
         this.productoCrudRepository.guardar(p);
     }
 
+    @RolesAllowed({"ADMIN"})
     @Override
     public void eliminar(Long id) {
         this.productoCrudRepository.eliminar(id);
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public List<Categoria> listarCategorias() {
         return this.categoriaCrudRepository.listar();
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public Optional<Categoria> porIdCategoria(Long id) {
         return Optional.ofNullable(this.categoriaCrudRepository.porId(id));
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public List<Producto> buscarPorNombre(String nombre) {
         return this.productoCrudRepository.buscarPorNombre(nombre);
